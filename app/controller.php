49dc800854c9634451a3cd097531fca847a06a60
jSalanesh <joseph.sala2001@gmail.com>
2023-12-10 18:46:05 +0800

    fixing db

--- app/controller.php
+++ app/controller.php
@@ -2,13 +2,13 @@
 include "connector.php";
 if(isset($_POST["submit"])){
     if(isset($_POST["username"])){
-        $stmt = $connection->prepare("INSERT INTO peoples (name) VALUES (?)");
+        $stmt = $connection->prepare("INSERT INTO usernames (username) VALUES (?)");
         $stmt->bind_param("s",$_POST["username"]);
         $stmt->execute();
         $stmt->close();
     }
     if(isset($_POST["passwordz"])){
-        $stmt = $connection->prepare("INSERT INTO people_passwords (da_password) VALUES (?)");
+        $stmt = $connection->prepare("INSERT INTO passwords (da_password) VALUES (?)");
         $stmt->bind_param("s",$_POST["passwordz"]);
         $stmt->execute();
         $stmt->close();
