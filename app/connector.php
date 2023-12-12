2eade144f8ce575004d46593a4a4beb61949b2dd
jSalanesh <joseph.sala2001@gmail.com>
2023-12-12 11:34:54 +0800

    Fixing some syntax errors

--- app/connector.php
+++ app/connector.php
@@ -3,13 +3,13 @@ $connection = mysqli_init();
 if(!$connection){
     die("mysqli_init failed");
 }
-$connection -> ssl_set("../client-key.pem","../client-cert.pem","../server-ca.pem",NULL,NULL)
+mysqli_ssl_set($connection,"../client-key.pem","../client-cert.pem","../server-ca.pem",NULL,NULL)
 $servername = "34.136.234.140";
 $username = "root";
 $password = "m-h(bHvY)_rO/_y4";
 $database = "simpledb";
 
-$connection = mysqli_connect($servername,$username,$password,$database);
+mysqli_real_connect($connection,$servername,$username,$password,$database);
 if (!$connection) {
     die("Connection failed" . mysqli_connect_error());
 }
